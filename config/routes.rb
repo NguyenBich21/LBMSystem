23962ce38e4a61e1d2604ae7e5f90125f50b4402
NguyenBich21 <vantang200378@gmail.com>
2023-12-23 20:50:04 +0700

    Add rails admin

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     mount Sidekiq::Web => '/sidekiq'
   end
 
-
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
