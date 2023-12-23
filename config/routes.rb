6409af3c4fa52d1663406b9832e6d19f6eb8a5f1
NguyenBich21 <vantang200378@gmail.com>
2023-12-24 03:38:30 +0700

    Polishing plate boiler content

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "home#index"
+  root "books#index"
   resources :books
   resources :book_borrows
   resources :publishing_companies
